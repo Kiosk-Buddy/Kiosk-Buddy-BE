71b759ee3176e5ce355ab0387a61128ea61cd6f1
Seungwon-Choi <bestwondev@gmail.com>
2025-04-03 22:57:58 +0900

    fix: ErrorCode 내부의 오타 수정

--- src/main/java/com/kioskbuddy/common/exception/ErrorCode.java
+++ src/main/java/com/kioskbuddy/common/exception/ErrorCode.java
@@ -7,7 +7,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    INTERNAL_SEVER_ERROR(500, "서버 에러가 발생했습니다. 관리자에게 문의 바랍니다.");
+    // Client
+    INVALID_AGE(422, "나이는 1 이상 99 이하로 입력해야 합니다."),
+    INVALID_PHONE_NUMBER(422, "전화번호 형식이 알맞지 않습니다."),
+    INVALID_PASSWORD(400, "비밀번호는 최소 8자 이상이며, 영문과 숫자를 포함해야 합니다."),
+
+    // Server
+    INTERNAL_SERVER_ERROR(500, "서버 에러가 발생했습니다. 관리자에게 문의 바랍니다.");
 
     private final Integer code;
     private final String message;
