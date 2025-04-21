c9e67700b1322fefb5f47e23104ed5f95ed63528
Seungwon-Choi <bestwondev@gmail.com>
2025-04-21 18:37:18 +0900

    initial commit #1

---
+++ src/main/java/com/kioskbuddy/backend/KioskBuddyBeApplication.java
@@ -0,0 +1,13 @@
+package com.kioskbuddy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskBuddyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskBuddyBeApplication.class, args);
+    }
+
+}
