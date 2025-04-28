cab60598e4455627564656a54ee0edbc6c51c4ae
Seungwon-Choi <bestwondev@gmail.com>
2025-04-28 21:45:16 +0900

    test: Member Test 추가 #5

--- src/test/java/com/kioskbuddy/backend/member/domain/MemberTest.java
+++ src/test/java/com/kioskbuddy/backend/member/domain/MemberTest.java
@@ -1,5 +1,6 @@
 package com.kioskbuddy.backend.member.domain;
 
+import com.kioskbuddy.backend.member.application.dto.MemberUpdateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ class MemberTest {
     
     @Test
     @DisplayName("Member 생성 테스트")
-    void createMember() {
+    void createMemberTest() {
         // given
         String name = "홍길동";
         Integer age = 30;
@@ -17,12 +18,7 @@ class MemberTest {
         String password = "Abc123!@#";
 
         // when
-        Member member = Member.builder()
-                .name(name)
-                .age(age)
-                .phoneNumber(phoneNumber)
-                .password(password)
-                .build();
+        Member member = Member.create(name, age, phoneNumber, password);
 
         // then
         assertEquals(name, member.getName());
@@ -30,4 +26,26 @@ class MemberTest {
         assertEquals(phoneNumber, member.getPhoneNumber());
         assertEquals(password, member.getPassword());
     }
+    
+    @Test
+    @DisplayName("Member 업데이트 테스트")
+    void updateMemberTest() {
+        // given
+        String name = "홍길동";
+        Integer age = 30;
+        String phoneNumber = "010-1234-5678";
+        String password = "Abc123!@#";
+        Member member = Member.create(name, age, phoneNumber, password);
+        
+        // when
+        String newName = "김철수";
+        Integer newAge = 35;
+        String newPhoneNumber = "010-9876-5432";
+        member.update(new MemberUpdateRequest(newName, newAge, newPhoneNumber));
+        
+        // then
+        assertEquals(newName, member.getName());
+        assertEquals(newAge, member.getAge());
+        assertEquals(newPhoneNumber, member.getPhoneNumber());
+    } 
 }
