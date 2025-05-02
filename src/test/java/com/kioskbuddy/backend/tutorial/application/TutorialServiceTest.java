8fc61d4426c95a0c557f0c49cd9263f05f8d0d21
Seungwon-Choi <bestwondev@gmail.com>
2025-05-02 09:43:09 +0900

    test: Create Tutorial 테스트 추가 #8

---
+++ src/test/java/com/kioskbuddy/backend/tutorial/application/TutorialServiceTest.java
@@ -0,0 +1,64 @@
+package com.kioskbuddy.backend.tutorial.application;
+
+import com.kioskbuddy.backend.tutorial.application.dto.TutorialCreateRequest;
+import com.kioskbuddy.backend.tutorial.domain.DifficultyLevel;
+import com.kioskbuddy.backend.tutorial.domain.Tutorial;
+import com.kioskbuddy.backend.tutorial.repository.JpaTutorialRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class TutorialServiceTest {
+
+    @Mock
+    private JpaTutorialRepository tutorialRepository;
+
+    @InjectMocks
+    private TutorialService tutorialService;
+
+    @Test
+    @DisplayName("Tutorial 등록 테스트")
+    void registerTutorialTest() {
+        // given
+        String title = "테스트 튜토리얼";
+        String description = "이 튜토리얼은 테스트용입니다.";
+        DifficultyLevel difficultyLevel = DifficultyLevel.EASY;
+
+        TutorialCreateRequest request = new TutorialCreateRequest(title, description, difficultyLevel);
+        Long expectedId = 1L;
+
+        Tutorial tutorial = Tutorial.builder()
+                .title(title)
+                .description(description)
+                .difficultyLevel(difficultyLevel)
+                .build();
+
+        // id를 설정한 Tutorial 객체를 반환하도록 설정
+        Tutorial savedTutorial = Tutorial.builder()
+                .title(title)
+                .description(description)
+                .difficultyLevel(difficultyLevel)
+                .build();
+        ReflectionTestUtils.setField(savedTutorial, "id", expectedId); // ReflectionTestUtils로 id 설정
+
+        // when
+        given(tutorialRepository.save(any(Tutorial.class))).willReturn(savedTutorial);
+        Long tutorialId = tutorialService.createTutorial(request);
+
+        // then
+        assertEquals(expectedId, tutorialId);
+        assertEquals(title, savedTutorial.getTitle());
+        assertEquals(description, savedTutorial.getDescription());
+        assertEquals(difficultyLevel, savedTutorial.getDifficultyLevel());
+        assertEquals(expectedId, savedTutorial.getId());
+    }
+}
