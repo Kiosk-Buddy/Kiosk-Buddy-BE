f8ee27b880ea07c5c9e4b20cef4725c734f947c6
Seungwon-Choi <bestwondev@gmail.com>
2025-05-02 10:12:46 +0900

    fix: DifficultyLevel 내 메소드 수정 #8

--- src/main/java/com/kioskbuddy/backend/tutorial/domain/DifficultyLevel.java
+++ src/main/java/com/kioskbuddy/backend/tutorial/domain/DifficultyLevel.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 
 @RequiredArgsConstructor
 public enum DifficultyLevel {
@@ -11,13 +13,16 @@ public enum DifficultyLevel {
     MEDIUM,
     HARD;
 
-    @JsonCreator
-    public static DifficultyLevel from(String value) {
-        return DifficultyLevel.valueOf(value.toUpperCase());
-    }
-
     @JsonValue
     public String toValue() {
         return this.name().toLowerCase();
+    }
+
+    @JsonCreator
+    public static DifficultyLevel from(String value) {
+        return Arrays.stream(DifficultyLevel.values())
+                .filter(e -> e.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("허용되지 않는 난이도입니다: " + value));
     }
 }
