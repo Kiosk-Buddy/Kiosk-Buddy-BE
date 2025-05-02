fe19474d0d75c9d84040cf70a35a969052c892ff
Seungwon-Choi <bestwondev@gmail.com>
2025-05-02 10:12:14 +0900

    fix: 응답 Dto 수정 #8

--- src/main/java/com/kioskbuddy/backend/tutorial/application/dto/TutorialDetailResponse.java
+++ src/main/java/com/kioskbuddy/backend/tutorial/application/dto/TutorialDetailResponse.java
@@ -1,19 +1,20 @@
 package com.kioskbuddy.backend.tutorial.application.dto;
 
+import com.kioskbuddy.backend.tutorial.domain.DifficultyLevel;
 import com.kioskbuddy.backend.tutorial.domain.Tutorial;
 
 public record TutorialDetailResponse(
 
         String title,
         String description,
-        String difficultyLevel
+        DifficultyLevel difficultyLevel
 ) {
 
     public TutorialDetailResponse(Tutorial tutorial) {
         this(
                 tutorial.getTitle(),
                 tutorial.getDescription(),
-                tutorial.getDifficultyLevel().toValue()
+                tutorial.getDifficultyLevel()
         );
     }
 }
