7af0e40c68c9576ca5a4acae8a263ea01d60c882
Seungwon-Choi <bestwondev@gmail.com>
2025-05-16 10:23:31 +0900

    fix: Member 관련 로직 수정 #20

--- src/main/java/com/kioskbuddy/backend/member/application/dto/MemberDetailResponse.java
+++ src/main/java/com/kioskbuddy/backend/member/application/dto/MemberDetailResponse.java
@@ -1,19 +1,22 @@
 package com.kioskbuddy.backend.member.application.dto;
 
 import com.kioskbuddy.backend.member.domain.Member;
+import com.kioskbuddy.backend.member.domain.MemberType;
 
 public record MemberDetailResponse(
         Long id,
         String name,
         Integer age,
-        String phoneNumber
+        String phoneNumber,
+        MemberType memberType
 ) {
     public static MemberDetailResponse from(Member member) {
         return new MemberDetailResponse(
                 member.getId(),
                 member.getName(),
                 member.getAge(),
-                member.getPhoneNumber()
+                member.getPhoneNumber(),
+                member.getMemberType()
         );
     }
 }
